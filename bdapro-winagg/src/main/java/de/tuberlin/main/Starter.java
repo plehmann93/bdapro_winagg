24875dd90a3354617348396e04f933721e230ab7
plehmann93 <pat@my-lehmann.de>
2017-01-12 19:21:59 +0100

    added again old class

--- bdapro-winagg/src/main/java/de/tuberlin/main/Starter.java
+++ bdapro-winagg/src/main/java/de/tuberlin/main/Starter.java
@@ -32,7 +32,7 @@ public class Starter {
             if(conf.getClusterMode()==1){
                 new FlinkWindowFromKafkaCluster(conf);
             }else{
-                new FlinkWindowFromKafka2(conf);
+                new FlinkWindowFromKafka(conf);
             }
 
 
